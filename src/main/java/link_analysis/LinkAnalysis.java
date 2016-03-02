b20cc85589cf41d495423eef34baf1545c496670
c1rabbit <mail@calvinthanh.com>
2016-03-02 01:40:31 -0800

    3 iteration link analysis might be working

--- src/main/java/link_analysis/LinkAnalysis.java
+++ src/main/java/link_analysis/LinkAnalysis.java
@@ -3,6 +3,8 @@ package link_analysis;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,13 +34,17 @@ public class LinkAnalysis {
 		}
 		JSONObject json = (JSONObject) obj;
 		this.outLinksCollection = json;
+		JSONArray outdocs = (JSONArray) json.get("db");
+		for (int i = 0; i < this.outLinksCollection.size(); i++) {
+			this.outLinksCollection.get("db");
+		}
 		System.out.println("out links: " + this.outLinksCollection);
 
 		// set new collection
 		inLinksCollection = new JSONObject();
 		inLinksCollection.put("db", new JSONArray());
 
-		buildInLinksCollection();
+		run();
 	}
 
 	public void insertLinkRecord(String docName, String link) {
@@ -64,7 +70,6 @@ public class LinkAnalysis {
 			document.put("name", docName);
 			document.put("links", links);
 			collection.add(document);
-
 		}
 
 	}
@@ -75,24 +80,108 @@ public class LinkAnalysis {
 		for (int i = 0; i < outlinks.size(); i++) {
 			JSONObject doc = (JSONObject) outlinks.get(i);
 			String src = (String) doc.get("name");
-			List<String> links = (List<String>) doc.get("link");
+			List<String> links = (List<String>) doc.get("links");
 			for (String dest : links) {
 				insertLinkRecord(dest, src);
 			}
 		}
 
 		// finalize jsonarray
-
 		System.out.println("in links: " + inLinksCollection);
 
 	}
 
 	public void run() {
+		buildInLinksCollection();
 
 		// get total number of documents
-		// int docCount = collection.size();
-		int docCount = 0;
+		JSONArray docs = (JSONArray) outLinksCollection.get("db");
+
+		double docCount = docs.size();
+		Map<String, Double> rank = new HashMap<String, Double>();
+		for (int i = 0; i < docCount; i++) {
+			JSONObject doc = (JSONObject) docs.get(i);
+			rank.put(doc.get("name").toString(), (double) (1 / docCount));
+		}
+		System.out.println("initial rank:\t" + rank);
+
+		JSONArray inLinks = (JSONArray) inLinksCollection.get("db");
+
+		// iterate and rank through each document
+		Map<String, Double> temp = new HashMap<String, Double>();
+		for (String key : rank.keySet()) {
+			temp.put(key, 0.0);
+		}
+		System.out.println("tempRank:\t" + temp);
+
+		int iteration = 0;
+		while (iteration < 3) {
+
+			for (int i = 0; i < inLinks.size(); i++) {
+				JSONObject j = (JSONObject) inLinks.get(i);
+				String name = (String) j.get("name");
+				List<String> ilinks = (List<String>) j.get("links");
+
+				double tempRank = 0.0;
+				for (String l : ilinks) {
+
+					int size = 0;
+					// calculate the outlink rank
+					for (int k = 0; k < docs.size(); k++) {
+						JSONObject obj = (JSONObject) docs.get(k);
+
+						if (obj.get("name").equals(l)) {
+							List<String> o = (List<String>) obj.get("links");
+							size = o.size();
+
+						}
+
+					}
+					System.out.println(l + " outlink count: " + size);
+					double currentRank = rank.get(name) / size;
+					tempRank += currentRank;
+					System.out.println(l + ": +" + currentRank);
+				}
+				System.out.println("--rank: " + name + " " + tempRank);
+				temp.put(name, tempRank);
+
+			}
+
+			// set temp ranking to final rank set
+			rank = temp;
+			System.out.println("iteration: " + iteration + " " + rank);
+			iteration++;
+		}
+
+		/*
+		 * for (String key : rank.keySet()) { // get rank per key(document)
+		 * double value = 0.0; // System.out.println(docs);
+		 * 
+		 * temp.put(key, value);
+		 * 
+		 * }
+		 */
+
+		/*
+		 * for (int i = 0; i < 4; i++) {
+		 * 
+		 * for (int j = 0; j < docCount; j++) { JSONObject document =
+		 * (JSONObject) docs.get(j); List<String> links = (LinkedList<String>)
+		 * document.get("link"); for (String link : links) {
+		 * temp.put(document.get("name").toString(),
+		 * temp.get(document.get("name"))); } } for (String document :
+		 * rank.keySet()) {
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * for (String key : rank.keySet()) {
+		 * 
+		 * double rankValue = rank.get(key);
+		 * 
+		 * }
+		 */
 
 	}
-
 }
